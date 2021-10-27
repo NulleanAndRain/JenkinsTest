d32b1671aa581d5be43568546bcefb6cec5ffef3
Nullean <mr000lean@gmail.com>
2021-10-27 20:47:42 +0400

    changed build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,1 @@
-
-C:\"Program Files (x86)"\"Microsoft Visual Studio"\2019\Community\MSBuild\Current\Bin\MSBuild.exe "Test\Test.csproj" -property:Configuration=Debug -noLogo
+C:\"Program Files (x86)"\"Microsoft Visual Studio"\2019\Community\MSBuild\Current\Bin\MSBuild.exe Test.sln
