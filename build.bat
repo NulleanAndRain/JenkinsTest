6da02e498d738e49e96c64802f1f2266c001b3d0
Nullean <mr000lean@gmail.com>
2021-10-27 21:59:25 +0400

    added nuget restore to build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Test.sln /p:Configuration=Release /restore
+G:\Jenkins\nuget.exe restore Test.sln
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe Test.sln /p:Configuration=Release
