6f492ba976fc56200dbebd7220d1bed8046d5203
NulleanAndRain <79399864+NulleanAndRain@users.noreply.github.com>
2021-10-27 22:54:32 +0400

    Update tests.bat

--- tests.bat
+++ tests.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "Test.Tests\bin\Release\Test.Tests.dll"
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\TestProject\Test.Tests\bin\Release\Test.Tests.dll"
