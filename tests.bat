b2de186e557bc297f2deb0bf7ee2d728506b5af8
NulleanAndRain <79399864+NulleanAndRain@users.noreply.github.com>
2021-12-25 19:58:10 +0300

    Update tests.bat

--- tests.bat
+++ tests.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "Test.Tests\bin\Release\Test.Tests.dll"
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" "Test.Tests\bin\Release\Test.Tests.dll"
